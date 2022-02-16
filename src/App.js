b25d73647c27fef2d0bbfb02d73dfc25eff22217
Joey <javascriptjoey@outlook.com>
2022-02-15 22:19:56 -0800

    Header component is working and set up

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 
 import HomePage from './pages/homepage/Homepage.component'
 import ShopPage from './pages/shop/shop.component';
-
+import Header from './components/header/Header.component';
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage}/>     
